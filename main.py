fa3d9823c40434fd793fb79ae2e3565f1e07fee4
Toms Gailis <tomsgailis29@inbox.lv>
2022-02-13 19:26:29 +0200

    Added AI Lap counter

--- main.py
+++ main.py
@@ -91,14 +91,14 @@ while True:
                 0 if 0 in PB_Sector else sum(PB_Sector)
             print(*[f'{floor(Value / (1000 * 60)):>1d}:{(Value / 1000) % 60:>06.3f}' for Value in PB_List[:10]])
             print(*[f'{(Value / 1000) % 60:>06.3f}' for Value in PB_Sector])
-            print(f'Personal :'
-                  f'{floor(SOB / (1000 * 60)):>6d}:{(SOB / 1000) % 60:>06.3f}'
+            print(f'{"Personal :":<21s}'
+                  f'{floor(SOB / (1000 * 60)):>1d}:{(SOB / 1000) % 60:>06.3f}'
                   f'|{floor(PB_Time / (1000 * 60)):>1d}:{(PB_Time / 1000) % 60:>06.3f}'
                   f'|{floor(PB3_Time / (1000 * 60)):>1d}:{(PB3_Time / 1000) % 60:>06.3f}'
                   f'|{floor(PB10_Time / (1000 * 60)):>1d}:{(PB10_Time / 1000) % 60:>06.3f}'
-                  f'|{floor(AVG / (1000 * 60)):>1d}:{(AVG / 1000) % 60:>06.3f}')
-            print('-' * 60)
-            print(f'{"AI":^4s}{"Time":^10s}{"SoB":>7s}{"PB":>9s}{"AVG-T3":>11s}{"AVG-T10":>9s}{"AVG":>7s}')
+                  f'|{floor(AVG / (1000 * 60)):>1d}:{(AVG / 1000) % 60:>06.3f}\n'
+                  + f'-' * 65 +
+                  f'\n{"AI":>9s}{"Time":>8s}{"SoB":>10s}{"PB":>9s}{"AVG-T3":>11s}{"AVG-T10":>9s}{"AVG":>7s}')
 
             Filtered = [(Key, Value['Time']) for Key, Value in AI_Times.items() if not Value['Generated']]
             for Current, Next in zip_longest(sorted(Filtered), sorted(Filtered)[1:]):
@@ -112,15 +112,15 @@ while True:
                     Difficulty = CurrentAI + Step
                     AI_Times.update(
                         {Difficulty: {'Time': [Time] if Generated else CurrentTime, 'Generated': Generated}})
-                    print(f'{"*" + str(Difficulty) if Generated else Difficulty:^4}'
+                    print(f'{"" if Generated else "(" + str(len(CurrentTime)) + ")":>5} '
+                          f'{"*" + str(Difficulty) if Generated else Difficulty:^4}'
                           f'{floor(Time / (1000 * 60)):>2d}:{(Time / 1000) % 60:06.3f}'
                           f'{(SOB - Time) / 1000:>+10.3f}'
                           f'{(PB_Time - Time) / 1000:>+9.3f}'
                           f'{(PB3_Time - Time) / 1000:>+9.3f}'
                           f'{(PB10_Time - Time) / 1000:>+9.3f}'
                           f'{(AVG - Time) / 1000:>+9.3f}')
-
-            print('Suggested:{recSOB:>11}{recPB:>9}{recPB3:>9}{recPB10:>9}{recAVG:>9}'.format(
+            print('Suggested:{recSOB:>17}{recPB:>9}{recPB3:>9}{recPB10:>9}{recAVG:>9}'.format(
                 recPB=min([Key for Key, Value in AI_Times.items() if PB_Time - mean(Value['Time']) > 0],
                           default=max(AI_Times)),
                 recPB3=min([Key for Key, Value in AI_Times.items() if PB3_Time - mean(Value['Time']) > 0],
@@ -130,8 +130,7 @@ while True:
                 recAVG=min([Key for Key, Value in AI_Times.items() if AVG - mean(Value['Time']) > 0],
                            default=max(AI_Times)),
                 recSOB=min([Key for Key, Value in AI_Times.items() if SOB - mean(Value['Time']) > 0],
-                           default=max(AI_Times))))
-            print('Waiting for results ...')
+                           default=max(AI_Times))) + f'\nWaiting for results ...')
 
         case 11:
             if SessionData.get('Type') != 9 or SessionData.get('Event') != 'SEND':
